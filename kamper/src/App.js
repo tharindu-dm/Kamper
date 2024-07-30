5e82ab68e0fc8ccd303b9a05fc5335094107ad56
Tharindu D Munasinghe <85168846+tharindu-dm@users.noreply.github.com>
2024-07-30 16:46:21 +0000

    .

--- kamper/src/App.js
+++ kamper/src/App.js
@@ -15,14 +15,6 @@ function App() {
             <li>Praneesh</li>
           </ul>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
