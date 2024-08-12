2aa5a16e43117fd15e8cfd17d469b7f650f4bbc8
Naveen Harinda <naveenharinda2@gmail.com>
2024-08-12 09:51:49 +0530

    Naveen's first commit
    Added Landing Page with Header
    Set up app.js with routes and layout and usercontext.
    Set up tailwind css
    Connected mongodb with node/express
    implemented api
    Implemented Login, Registration, Logout logic with proper api calls.
    Implemented Account page.

---
+++ api/index.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("./models/user.js");
+const CookieParser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const app = express();
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = "fasefraw4r5r3wq45wdfgw34twdfg";
+("");
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
+
+function getUserDataFromReq(req) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      resolve(userData);
+    });
+  });
+}
+
+app.get("/test", (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  res.json("test ok");
+});
+
+app.post("/register", async (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
+
+  const { name, email, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    console.log("catched");
+    res.status(422).json(e);
+  }
+});
+
+app.post("/api/login", async (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const { email, password } = req.body;
+
+  // Check if the user exists
+  const userDoc = await User.findOne({ email });
+
+  if (!userDoc) {
+    // If the user doesn't exist, send a 401 Unauthorized response
+    return res.status(401).json({ error: "User not found" });
+  }
+
+  // If the user exists, verify the password
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+
+  if (!passOk) {
+    // If the password is incorrect, send a 401 Unauthorized response
+    return res.status(401).json({ error: "Invalid password" });
+  }
+
+  // If authentication is successful, generate and send a JWT token
+  jwt.sign(
+    {
+      email: userDoc.email,
+      id: userDoc._id,
+    },
+    jwtSecret,
+    {},
+    (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json(userDoc);
+    }
+  );
+});
+
+app.get("/api/profile", (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const { token } = req.cookies;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      const { name, email, _id } = await User.findById(userData.id);
+      res.json({ name, email, _id });
+    });
+  } else {
+    res.json(null);
+  }
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json(true);
+});
+app.listen(4000);
