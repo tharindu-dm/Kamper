b119fe1c4fc5a1cfbf3a66b37cf128b8f3ec06fe
tharindu-dm <85168846+tharindu-dm@users.noreply.github.com>
2024-08-20 02:19:41 +0530

    images not showing error fixed (https -> http), error related to not having a unique key fixed, at line 42 div. edited if at line 24. totalprice at line 30
    Co-authored-by: tharindu-dm <85168846+tharindu-dm@users.noreply.github.com>
    Co-authored-by: Tharindu D Munasinghe <85168846+tharindu-dm@users.noreply.github.com>
    Co-authored-by: ravidu001 <ravidupeiris2002@gmail.com>

--- client/src/BookingWidget.js
+++ client/src/BookingWidget.js
@@ -5,86 +5,102 @@ import { UserContext } from "./userContext"; /* path will be updated */
 import axios from "axios";
 import { differenceInCalendarDays } from "date-fns";
 
-export default function BookingWidget(place) {
-    const[checkIn, setCheckIn] = useState('');
-    const[checkOut, setCheckOut] = useState('');
-    const[numberofGuests, setNumberOfGuests] = useState(1);
-    const [name,setName] = useState('');
-    const [phone, setPhone] = useState('');
-    const [redirect, setRedirect] = useState('');
-    const {user} = useContext(UserContext); 
+export default function BookingWidget({ place }) {
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [numberOfGuests, setNumberOfGuests] = useState(1);
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [redirect, setRedirect] = useState("");
+  const { user } = useContext(UserContext);
 
-    useEffect(() => {
-        if (user) {
-            setName(user.name);    
-        }
-    }, [user]);
-    
-    let numberOfNights = 0;
-    if (checkIn && setCheckOut){
-        numberOfNights = differenceInCalendarDays(new Date(checkOut), new Date(checkIn));
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
     }
-    
-    async function bookThisPlace() {
-        const response = await axios.post('/bookings', {
-            checkIn, checkOut, numberofGuests, name, phone, 
-            place:place._id, 
-            price: numberOfNights * place.price,
-        });
-        const bookingId = response.data._id;
-        setRedirect(`/account/bookings/${bookingId}`);
-    }
+  }, [user]);
 
-    if (redirect) {
-        return <Navigate to={redirect} />
-    }
-    
-    
-    return (
-        <div className="bg-white shadow p-4 rounded-2xl">
-            <div className="text-2xl text-center">
-                Price: ${place.price} / per night     
-            </div>
-            <div className="border rounded-2xl mt-4">
-                <div className="flex">
-                    <div className="py-3 px-4">
-                        <label>Check-in:</label>
-                        <input type="date" 
-                            value={checkIn} 
-                            onChange={ev => setCheckIn(ev.target.value)} />
-                    </div>
-                    <div className="py-3 px-4 border-l">
-                        <label>Check-out:</label>
-                        <input type="date" 
-                            value={checkOut} 
-                            onChange={ev => setCheckOut(ev.target.value)} />
-                    </div>
-                </div>
-                <div className="py-3 px-4 border-t">
-                    <label>Number of guests:</label>
-                    <input type="number" 
-                        value={numberofGuests} 
-                        onChange={ev => setNumberOfGuests(ev.target.value)} />
-                </div>
-                {numberOfNights > 0 && (
-                    <div className="py-3 px-4 border-t">
-                        <label>Your full name:</label>
-                        <input type="text" 
-                            value={name} 
-                            onChange={ev => setName(ev.target.value)} />
-                        <label>Phone number:</label>
-                        <input type="tel" 
-                            value={phone} 
-                            onChange={ev => setPhone(ev.target.value)} />
-                    </div>
-                )}
-            </div>
-            <button onClick={bookThisPlace} className="primary mt-4">
-                Book this place
-                {numberOfNights > 0 && (
-                    <span> ${numberOfNights * place.price}</span>
-                )}
-            </button>
-        </div>
+  let numberOfNights = 0;
+  if (checkIn && checkOut) {
+    numberOfNights = differenceInCalendarDays(
+      new Date(checkOut),
+      new Date(checkIn)
     );
+  }
+  const totalPrice =
+    numberOfNights > 0 ? numberOfNights * place.price : place.price;
+
+  async function bookThisPlace() {
+    const response = await axios.post("/bookings", {
+      checkIn,
+      checkOut,
+      numberOfGuests,
+      name,
+      phone,
+      place: place._id,
+      price: totalPrice,
+    });
+    const bookingId = response.data._id;
+    setRedirect(`/account/bookings/${bookingId}`);
+  }
+
+  if (redirect) {
+    return <Navigate to={redirect} />;
+  }
+
+  return (
+    <div className="bg-white shadow p-4 rounded-2xl">
+      <div className="text-2xl text-center">
+        Price: ${place.price} / per night
+      </div>
+      <div className="border rounded-2xl mt-4">
+        <div className="flex">
+          <div className="py-3 px-4">
+            <label>Check-in:</label>
+            <input
+              type="date"
+              value={checkIn}
+              onChange={(ev) => setCheckIn(ev.target.value)}
+            />
+          </div>
+          <div className="py-3 px-4 border-l">
+            <label>Check-out:</label>
+            <input
+              type="date"
+              value={checkOut}
+              onChange={(ev) => setCheckOut(ev.target.value)}
+            />
+          </div>
+        </div>
+        <div className="py-3 px-4 border-t">
+          <label>Number of guests:</label>
+          <input
+            type="number"
+            value={numberOfGuests}
+            onChange={(ev) => setNumberOfGuests(ev.target.value)}
+          />
+        </div>
+        {numberOfNights > 0 && (
+          <div className="py-3 px-4 border-t">
+            <label>Your full name:</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(ev) => setName(ev.target.value)}
+            />
+            <label>Phone number:</label>
+            <input
+              type="tel"
+              value={phone}
+              onChange={(ev) => setPhone(ev.target.value)}
+            />
+          </div>
+        )}
+      </div>
+      <button onClick={bookThisPlace} className="primary mt-4">
+        Book this place
+        {numberOfNights > 0 && <span> ${numberOfNights * place.price}</span>}
+      </button>
+    </div>
+  );
 }
--- client/src/pages/PlacePage.js
+++ client/src/pages/PlacePage.js
@@ -1,109 +1,153 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 import BookingWidget from "../BookingWidget";
-import CloseIcon from '@mui/icons-material/Close';
-import CropOriginalIcon from '@mui/icons-material/CropOriginal';
+import CloseIcon from "@mui/icons-material/Close";
+import CropOriginalIcon from "@mui/icons-material/CropOriginal";
 import axios from "axios";
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 export default function PlacePage() {
-    const {id} = useParams();
-    const [place, setPlace] = useState(null);
-    const[showAllPhotos, setShowAllPhotos] = useState(false);
-    useEffect(() => {
-        if (!id) {
-            return;
-        }
-        axios.get(`/api/places/${id}`).then((response) => {
-            setPlace(response.data);
-        });
-    }, [id]);  
-    
-    if (!place) {
-        return '';
+  const { id } = useParams();
+  const [place, setPlace] = useState(null);
+  const [showAllPhotos, setShowAllPhotos] = useState(false);
+  useEffect(() => {
+    if (!id) {
+      return;
     }
+    axios.get(`/api/places/${id}`).then((response) => {
+      setPlace(response.data);
+    });
+  }, [id]);
 
-    if (showAllPhotos){
-        return (
-            <div className="absolute inset-0 bg-black text-white min-h-screen">
-                <div className="bg-black p-8 grid gap-4">
-                    <div>
-                        <h2 className="text-3xl mr-48">Photos of {place.title}</h2>
-                        <button onClick={() => setShowAllPhotos(false)} className="fixed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl shadow-black bg-white text-black">
-                            <CloseIcon></CloseIcon>
-                            Close photos
-                        </button>
-                    </div>
-                    { place?.photos?.length > 0 && place.photos.map((photo) => (
-                        <div>
-                            <img src={"https://localhost:4000/uploads/"+photo} />
-                        </div>
-                    ))}
-                </div>   
-            </div>
-        )
-    }
-    
+  if (!place) {
+    return "";
+  }
+
+  if (showAllPhotos) {
     return (
-        <div className = "mt-4 bg-gray-100 -mx-8 px-8 pt-8">
-            <h1 className="text-3xl">{place.title}</h1>
-            <a className=" flex gap-1 my-3 block font-semibold underline" target="_blank" href={'https://maps.google.com/?q='+place.address}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-                </svg>
-
-                {place.address}</a>
-            <div className="relative">
-                <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden">
-                    <div>
-                        {place.photos?.[0] && (
-                            <div>
-                                <img onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover" src={"https://localhost:4000/uploads/"+place.photos[0]} />
-                            </div>
-                        )}
-                    </div>
-                        
-                    <div className="grid">
-                        {place.photos?.[1] && (
-                            <img onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover" src={"https://localhost:4000/uploads/"+place.photos[1]} />
-                        )}
-                        <div className="overflow-hidden"> {/* adjust the 2nd image bottom align with main image(01) */}
-                            {place.photos?.[2] && (
-                            <img onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover relative top-2" src={"https://localhost:4000/uploads/"+place.photos[2]} />
-                            )}
-                        </div>
-                    </div>
-                </div> 
-                <button onClick={() => setShowAllPhotos(true)} className="flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-500"> 
-                    <CropOriginalIcon></CropOriginalIcon>
-                    Show more photos
-                </button>   
-            </div>
-            
-            <div className="mt-8 mb-8 grid gap-8 grid-cols-1 md:grid-cols-[2fr_1fr]">
-                <div>
-                    <div className="my-4">
-                        <h2 className="font-semibold text-2xl">Description</h2>
-                        {place.description}
-                    </div>
-                    Check-in: {place.checkIn} <br />
-                    Check-out: {place.checkOut} <br />
-                    Max number of guests: {place.maxGuests} 
-                </div>
-                <div>
-                    <BookingWidget place={place}/>
-                </div>
-            </div>  
-            <div className="bg-white -mx-8 px-8 py-8 border-t">
-                <div>
-                    <h2 className="font-semibold text-2xl">Extra info</h2>
-                </div>
-                <div className="mb-4 mt-2 text-sm text-gray-700 leading-5">
-                    {place.extraInfo}
-                </div>  
-            </div>
-            
+      <div className="absolute inset-0 bg-black text-white min-h-screen">
+        <div className="bg-black p-8 grid gap-4">
+          <div>
+            <h2 className="text-3xl mr-48">Photos of {place.title}</h2>
+            <button
+              onClick={() => setShowAllPhotos(false)}
+              className="fixed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl shadow-black bg-white text-black"
+            >
+              <CloseIcon></CloseIcon>
+              Close photos
+            </button>
+          </div>
+          {place?.photos?.length > 0 &&
+            place.photos.map((photo) => (
+              <div key={photo}>
+                <img src={"http://localhost:4000/uploads/" + photo} alt="" />
+              </div>
+            ))}
         </div>
+      </div>
     );
+  }
+
+  return (
+    <div className="mt-4 bg-gray-100 -mx-8 px-8 pt-8">
+      <h1 className="text-3xl">{place.title}</h1>
+      <a
+        className=" flex gap-1 my-3 font-semibold underline"
+        target="_blank"
+        rel="noreferrer"
+        href={"http://maps.google.com/?q=" + place.address}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+          />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+          />
+        </svg>
+
+        {place.address}
+      </a>
+      <div className="relative">
+        <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden">
+          <div>
+            {place.photos?.[0] && (
+              <div>
+                <img
+                  onClick={() => setShowAllPhotos(true)}
+                  className="aspect-square cursor-pointer object-cover"
+                  src={"http://localhost:4000/uploads/" + place.photos[0]}
+                  alt=""
+                />
+              </div>
+            )}
+          </div>
+
+          <div className="grid">
+            {place.photos?.[1] && (
+              <img
+                onClick={() => setShowAllPhotos(true)}
+                className="aspect-square cursor-pointer object-cover"
+                src={"http://localhost:4000/uploads/" + place.photos[1]}
+                alt=""
+              />
+            )}
+            <div className="overflow-hidden">
+              {" "}
+              {/* adjust the 2nd image bottom align with main image(01) */}
+              {place.photos?.[2] && (
+                <img
+                  onClick={() => setShowAllPhotos(true)}
+                  className="aspect-square cursor-pointer object-cover relative top-2"
+                  src={"http://localhost:4000/uploads/" + place.photos[2]}
+                  alt=""
+                />
+              )}
+            </div>
+          </div>
+        </div>
+        <button
+          onClick={() => setShowAllPhotos(true)}
+          className="flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow-md shadow-gray-500"
+        >
+          <CropOriginalIcon></CropOriginalIcon>
+          Show more photos
+        </button>
+      </div>
+
+      <div className="mt-8 mb-8 grid gap-8 grid-cols-1 md:grid-cols-[2fr_1fr]">
+        <div>
+          <div className="my-4">
+            <h2 className="font-semibold text-2xl">Description</h2>
+            {place.description}
+          </div>
+          Check-in: {place.checkIn} <br />
+          Check-out: {place.checkOut} <br />
+          Max number of guests: {place.maxGuests}
+        </div>
+        <div>
+          <BookingWidget place={place} />
+        </div>
+      </div>
+      <div className="bg-white -mx-8 px-8 py-8 border-t">
+        <div>
+          <h2 className="font-semibold text-2xl">Extra info</h2>
+        </div>
+        <div className="mb-4 mt-2 text-sm text-gray-700 leading-5">
+          {place.extraInfo}
+        </div>
+      </div>
+    </div>
+  );
 }
